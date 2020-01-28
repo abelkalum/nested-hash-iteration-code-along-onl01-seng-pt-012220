851ef60413959d7e5361affc9b0f9c4c81e14e0e
Abel Kalum <chumbabel@gmail.com>
2020-01-28 13:39:17 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,7 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
+  
   contacts = {
   "Jon Snow" => {
     name: "Jon",
@@ -27,11 +28,5 @@ def remove_strawberry(contacts)
     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   }
 }
-contacts.each do |person, contact_details_hash|
-  if person == "Freddy Mercury"
-    contact_details_hash.each do |attribute, data|
-      binding.pry
-    end
-  end
-end
+
 end
