a80bdd83b0f0cc5b54037dff6a5a8daf52d99ae1
Abel Kalum <chumbabel@gmail.com>
2020-01-28 13:31:18 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,9 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  
+   contacts.each do |person, contact_details_hash|
+   binding.pry
+ end
   contacts = {
   "Jon Snow" => {
     name: "Jon",
@@ -29,4 +31,5 @@ def remove_strawberry(contacts)
   }
 }
 
+ end
 end
